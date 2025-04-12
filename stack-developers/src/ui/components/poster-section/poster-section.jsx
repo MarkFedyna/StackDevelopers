4db57777434a93ef82ece1e6d031d14ba2a11ac1
Ostap <dmitrivostap8@gmail.com>
2025-04-12 17:46:59 +0300

    fixed images' paths in json file

--- stack-developers/src/ui/components/poster-section/poster-section.jsx
+++ stack-developers/src/ui/components/poster-section/poster-section.jsx
@@ -7,7 +7,7 @@ function PosterSection() {
     <section className="flex flex-col justify-between text-center gap-10 pt-9 px-12 pb-24">
       <h1 className="text-[#6A994E] text-5xl font-bold">Нові оголошення</h1>
 
-      <CardList cards={cardData} />
+      <CardList cards={cardData.splice(0, 4)} />
     </section>
   );
 }
