1711a9093a907d5750d870ef058281c8057b44c2
misha-fedyna <mihajlofedina5@gmail.com>
2025-04-12 17:25:29 +0300

    feat: changes into ui

--- stack-developers/src/ui/components/poster-section/poster-section.jsx
+++ stack-developers/src/ui/components/poster-section/poster-section.jsx
@@ -1,37 +1,11 @@
-import CardList from '@/src/ui/components/card-list/card-list';
-import dog1 from '@/public/dog2.png';
-
-const cardData = [
-  {
-    name: 'Барон',
-    description: 'Собака, 2 роки',
-    image: dog1,
-    id: '12ex74k',
-  },
-  {
-    name: 'Лорд',
-    description: 'Собака, 3 роки',
-    image: dog1,
-    id: '12ex4k',
-  },
-  {
-    name: 'Джессі',
-    description: 'Собака, 1 рік',
-    image: dog1,
-    id: '12ex44k',
-  },
-  {
-    name: 'Рокі',
-    description: 'Собака, 5 років',
-    image: dog1,
-    id: '12ex34k',
-  },
-];
+import CardList from "@/src/ui/components/card-list/card-list";
+import dog1 from "@/public/dog2.png";
+import cardData from "..//../../../petsData.json";
 
 function PosterSection() {
   return (
-    <section className='flex flex-col justify-between text-center gap-10 pt-9 px-12 pb-24'>
-      <h1 className='text-[#6A994E] text-5xl font-bold'>Нові оголошення</h1>
+    <section className="flex flex-col justify-between text-center gap-10 pt-9 px-12 pb-24">
+      <h1 className="text-[#6A994E] text-5xl font-bold">Нові оголошення</h1>
 
       <CardList cards={cardData} />
     </section>
